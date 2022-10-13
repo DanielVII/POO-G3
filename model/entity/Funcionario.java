e85cafa6a3b984ff6718c53a7106430c036f8dd6
welanio <wilgner_elanio@outlook.com>
2022-10-13 19:31:47 -0300

    nova classe

--- model/entity/Funcionario.java
+++ model/entity/Funcionario.java
@@ -1,42 +1,5 @@
 package model.entity;
 
-public class Funcionario{
-    private String nome,user,senha;
-
-    public void setNome(String nome){
-        //metodo setNome não permite que a String esteja vazia
-        if(nome != null){
-            if(nome.isEmpty()){
-                System.out.println("String nao pode ser vazia");
-            }
-            else{this.nome = nome;}
-        }
-    }
-    public void setUser(String user){
-        //metodo setUser não permite que a String esteja vazia
-        if(user != null){
-            if(user.isEmpty()){
-                System.out.println("String nao pode ser vazia");
-            }
-            else{this.user = user;}
-        }
-    }
-    public void setSenha(String senha){
-        //metodo setSenha não permite qu ela tenha menos que 8 caracteres
-        if(senha != null){
-            if(senha.length() < 8){
-                System.out.println("a senha deve conter no minimo 8 caracteres");
-            }
-            else{this.senha = senha;}
-        }   
-    }
-    public String getNome(){
-        return nome;
-    }
-    public String getUser(){
-        return user;
-    }
-    public String getSenha(){
-        return senha;
-    }
+public class Funcionario extends User{
+    
 }
